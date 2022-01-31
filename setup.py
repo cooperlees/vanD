b0195b4fefcef31b48d2568dc0781b614e177209
Cooper Ry Lees <me@cooperlees.com>
2022-01-30 21:04:01 -0800

    Move vand from src to avoid ptr bug ...

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import find_packages, setup
 
 
 ptr_params = {
-    "entry_point_module": "src/vand/main",
+    "entry_point_module": "vand/main",
     "test_suite": "vand.tests.base",
     "test_suite_timeout": 300,
     "required_coverage": {
-        "vand/main.py": 99,
+        "vand/main.py": 45,
+        "vand/li3.py": 30,
     },
     "run_black": True,
     "run_flake8": True,
@@ -21,8 +22,7 @@ setup(
     name="vand",
     version="2022.1.29",
     description=("Daemon for all your Van Life needs ..."),
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
     url="http://github.com/cooperlees/vanD/",
     author="Cooper Lees",
     author_email="me@cooperlees.com",
